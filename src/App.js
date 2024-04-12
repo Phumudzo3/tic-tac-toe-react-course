8ca2b42a01e2d3015e843f6f5a74a5f22faf0084
phumudzo <tshivhulas.p3@gmail.com>
2024-04-12 19:48:46 +0200

    added init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
-
+import { useContext } from "react";
+import Router from "./Router";
+import { GlobalStyles } from "./styles/Global.styled";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./styles/theme";
+import { ThemeContext } from "./contexts/ThemeContext";
+import MusicPlayer from "./components/MusicPlayer/MusicPlayer";
 
 function App() {
+  const { theme } = useContext(ThemeContext);
+  const mode = theme === "light" ? lightTheme : darkTheme;
+
   return (
-    <div className="App">
-      <h1>Tic tac toe</h1>
-    </div>
+    <ThemeProvider theme={mode}>
+      <GlobalStyles />
+      <Router />
+      <MusicPlayer />
+    </ThemeProvider>
   );
 }
 
